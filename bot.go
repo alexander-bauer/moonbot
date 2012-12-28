36885883c4cfdd0e6a8a3d098d5fb174a0f96d97
Sasha Crofter <sasha@crofter.org>
2012-12-28 00:56:02 -0500

    Add version command and channel simplification

--- bot.go
+++ bot.go
@@ -34,16 +34,21 @@ func NewBot(server, password string, logger *log.Logger, channels ...string) (bo
 			command := strings.SplitN(msg.Trailing, " ", 3)
 			if len(command) >= 2 && command[0] == Nick+":" {
 				var args string
+				channel := "#moonbot"
 				if len(command) >= 3 {
 					args = command[2]
 				}
 				switch strings.ToLower(command[1]) {
+				case "version":
+					b.Say("#moonbot", "Version "+Version+minversion)
+
 				case "quit":
 					b.l.Println("Quitting")
 					b.Quit()
+
 				case "echo":
 					b.l.Println("Echoing", args)
-					b.Say("#moonbot", args)
+					b.Say(channel, args)
 				}
 			}
 		}
